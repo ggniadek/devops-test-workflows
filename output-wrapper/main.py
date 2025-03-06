c76635eef8c35a12ab8459496e7acb228038ea05
Gonçalo <gteixei@uva.nl>
2025-03-06 17:27:11 +0100

    Designed and added wrapper for Lambda for variable passing.

---
+++ output-wrapper/main.py
@@ -0,0 +1,53 @@
+import json
+
+### For testing, must be removed!, this should be provided by Lambda.
+event_test_remove_in_deployment = {
+     "metadata": {
+          "names": [
+               "Ilia",
+               "Bruno",
+               "Gosia",
+               "Goncalo"
+          ]
+     }
+}
+
+vars_dict = {}
+def lambda_handler(event):
+     
+     # 1. Handling ingestion of previously defined variables
+     # This allows for the variables to be used.
+     metadata = event['metadata']
+     for key, value in metadata.items():
+          globals()[key] = value
+     
+          
+     #########
+     # Main body function goes here @ggnadiek :)
+     #########
+     
+     # 2. Handling export of current and previously defined variables;
+     # 3. Exporting variables (both from current and previous workflows)
+     # This is ensured by the `event` variable.
+     var_list = globals()
+     # Note G: this is a bit ugly, but it works. I'll refactor this after.
+     vars_dict_curr = { k: v for k, v in var_list.items() if not
+               k.startswith('__') |
+               k.startswith('json') |
+               k.startswith('lambda_handler') |
+               k.startswith('vars_dict') |
+               k.startswith('event_test_remove_in_deployment') # remove this in deployment, not needed
+          }
+     for key, value in vars_dict_curr.items():
+          if key != 'event':
+               if key == 'event':
+                    # Ensure that there's no overwritten data
+                    if vars_dict.get(key) == None:
+                         vars_dict.update(value['metadata'])
+               else:
+                    vars_dict.update({ key: value })
+     print(vars_dict)
+     
+lambda_handler(event_test_remove_in_deployment)
+     
+
