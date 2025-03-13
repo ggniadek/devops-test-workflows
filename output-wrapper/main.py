d694544e934dbfd28c23996b0d92c3be18d369aa
Gosia <m.k.gniadek@student.tudelft.nl>
2025-03-13 09:11:28 +0100

    reformating Goncalo's file

--- output-wrapper/main.py
+++ output-wrapper/main.py
@@ -1,49 +1,58 @@
 vars_dict = {}
+
 def lambda_handler(event, context):
-     
-     # 1. Handling ingestion of previously defined variables
-     # This allows for the variables to be used.
-     if 'metadata' in event:
-          metadata = event['metadata']
-          for key, value in metadata.items():
-               globals()[key] = value
-          
-     #########
-     # Main body function
-     #########
+    # Handling ingestion of previously defined variables
+    # Allows for the variables to be used.
+    if "metadata" in event:
+        metadata = event["metadata"]
+        for key, value in metadata.items():
+            globals()[key] = value
 
-     local_vars = locals()
-     local_vars_dict_curr = { k: v for k, v in local_vars.items() if not
-               k.startswith('key') |
-               k.startswith('value') |
-               # k.startswith('event') |
-               k.startswith('metadata') |
-               k.startswith('context')
-          }
+    #########
+    # Main body function
+    #########
 
-     # 2. Handling export of current and previously defined variables;
-     # 3. Exporting variables (both from current and previous workflows)
-     # This is ensured by the `event` variable.
-     var_list = globals()
-     # Note G: this is a bit ugly, but it works. I'll refactor this after.
-     vars_dict_curr = { k: v for k, v in var_list.items() if not
-               k.startswith('__') |
-               k.startswith('json') |
-               k.startswith('lambda_handler') |
-               k.startswith('vars_dict') |
-               k.startswith('context')
-          }
-     for key, value in vars_dict_curr.items():
-          if key == 'event':
-               # Ensure that there's no overwritten data
-               if vars_dict.get(key) == None:
-                    if 'metadata' in value:
-                         vars_dict.update(value['metadata'])
-          else:
-               vars_dict.update({ key: value })
-     for key, value in local_vars_dict_curr.items():
-          vars_dict.update({ key: value})
-     return {
-          "metadata": vars_dict
-     }
+    local_vars = locals()
+    local_vars_dict_curr = {
+        k: v
+        for k, v in local_vars.items()
+        if not (
+            k.startswith("key")
+            or k.startswith("value")
+            # or k.startswith("event")  # Commented out
+            or k.startswith("metadata")
+            or k.startswith("context")
+        )
+    }
 
+    # Handling export of current and previously defined variables
+    # Exporting variables (both from current and previous workflows)
+    var_list = globals()
+
+    vars_dict_curr = {
+        k: v
+        for k, v in var_list.items()
+        if not (
+            k.startswith("__")
+            or k.startswith("json")
+            or k.startswith("lambda_handler")
+            or k.startswith("vars_dict")
+            or k.startswith("context")
+        )
+    }
+
+    for key, value in vars_dict_curr.items():
+        if key == "event":
+            # Ensure that there's no overwritten data
+            if vars_dict.get(key) is None:
+                if "metadata" in value:
+                    vars_dict.update(value["metadata"])
+        else:
+            vars_dict.update({key: value})
+
+    for key, value in local_vars_dict_curr.items():
+        vars_dict.update({key: value})
+
+    return {
+        "metadata": vars_dict
+    }
