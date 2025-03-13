9c36537d4adde0cc845f599c1669f5d5dd8f3a32
Bruno Dzikowski <dzikowski.bruno@gmail.com>
2025-03-13 15:54:15 +0100

    Test

--- modularization/s3_uploader.py
+++ modularization/s3_uploader.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import boto3
 
-BUILD_DIR_PATH = Path('modularization', 'build')
+BUILD_DIR_PATH = Path('/home/runner/work/devops-test-workflows/devops-test-workflows/build')
 BUCKET_NAME = 'module-bucket-a60555b5-a452-46d5-8a9f-5248d2dc41a5'
 s3 = boto3.client('s3')
 
