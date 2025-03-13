856b599f10a958e0c20fcdd967eb2b6857741559
Bruno Dzikowski <dzikowski.bruno@gmail.com>
2025-03-13 16:21:26 +0100

    Fix S3 key

--- modularization/s3_uploader.py
+++ modularization/s3_uploader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import boto3
+from unicodedata import normalize
 
 BUILD_DIR_PATH = Path('build')
 BUCKET_NAME = 'module-bucket-a60555b5-a452-46d5-8a9f-5248d2dc41a5'
@@ -16,18 +17,16 @@ def list_zips(notebook_dir: Path) -> list[Path]:
         zips.append(path)
     return zips
 
-def upload_notebook_zips(zips: list[Path]):
+def upload_notebook_zips(notebook: Path, zips: list[Path]):
     for zip in zips:
-        s3.upload_file(zip, Bucket = BUCKET_NAME, Key = str(zip))
+        s3_key = f"{notebook.name}/{zip.name}"
+        s3.upload_file(zip, Bucket = BUCKET_NAME, Key = s3_key)
 
 def upload_zips():
-    test = Path('.').iterdir()
-    for t in test:
-        print(t)
     if not BUILD_DIR_PATH.exists():
         print("No build directory detected, exiting")
         return
     notebooks = [path for path in BUILD_DIR_PATH.iterdir() if path.is_dir()]
     for notebook in notebooks:
         zips = list_zips(notebook)
-        upload_notebook_zips(zips)
+        upload_notebook_zips(notebook, zips)
