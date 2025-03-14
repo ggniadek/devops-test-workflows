aebf1ea9df6f5fedfeac71b12c329907ab0d94ae
Bruno Dzikowski <dzikowski.bruno@gmail.com>
2025-03-14 11:43:53 +0100

    Add org name

--- modularization/s3_uploader.py
+++ modularization/s3_uploader.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import boto3
 from unicodedata import normalize
@@ -17,16 +18,23 @@ def list_zips(notebook_dir: Path) -> list[Path]:
         zips.append(path)
     return zips
 
-def upload_notebook_zips(notebook: Path, zips: list[Path]):
+def upload_notebook_zips(organization_name: str, notebook: Path, zips: list[Path]):
     for zip in zips:
-        s3_key = f"{notebook.name}/{zip.name}"
+        s3_key = f"{organization_name}/{notebook.name}/{zip.name}"
         s3.upload_file(zip, Bucket = BUCKET_NAME, Key = s3_key)
 
 def upload_zips():
     if not BUILD_DIR_PATH.exists():
         print("No build directory detected, exiting")
         return
+
+    organization_name = os.getenv("GITHUB_REPOSITORY_OWNER")
+    print(f"Github repository owner determined as {organization_name}")
+
     notebooks = [path for path in BUILD_DIR_PATH.iterdir() if path.is_dir()]
+    print(f"Found {len(notebooks)} notebooks")
     for notebook in notebooks:
         zips = list_zips(notebook)
-        upload_notebook_zips(notebook, zips)
+        print(f"Uploading {notebook.name} to S3...")
+        upload_notebook_zips(organization_name, notebook, zips)
+        print(f"Uploaded {notebook.name} to S3")
